8cf0c5a199ac78311b826936d281b9cbe781efd2
otabek005 <azamov521o@gmail.com>
2023-12-19 23:54:19 +0500

    Ashyo.uz.completed-site

---
+++ pages/savat/app.js
@@ -0,0 +1,55 @@
+  document.getElementById("aksiya").addEventListener("click", () => {
+    document.body.style.paddingTop = "0";
+    document.body.style.transition = "all 0.8s linear";
+    document.getElementById("aksiyaa").style.display = "block";
+    document.getElementById("headlist1").style.display = "none";
+    document.getElementById("headlist2").style.display = "none";
+    document.getElementById("headlist3").style.display = "none";
+    document.getElementById("headlist4").style.display = "none";
+    document.getElementById("headlist5").style.display = "none";
+    document.getElementById("headlist6").style.display = "none";
+    document.getElementById("headlist7").style.display = "none";
+    document.getElementById("headlist8").style.display = "none";
+  });
+  
+  
+  document.getElementById("main").addEventListener("click", () => {
+      document.body.style.paddingLeft = "0";
+      document.body.style.transition = "all 0.3s linear";
+      document.getElementById("aksiyaa").style.display = "none";
+      document.getElementById("headlist1").style.display = "block";
+      document.getElementById("headlist2").style.display = "block";
+      document.getElementById("headlist3").style.display = "block";
+      document.getElementById("headlist4").style.display = "block";
+      document.getElementById("headlist5").style.display = "block";
+      document.getElementById("headlist6").style.display = "block";
+      document.getElementById("headlist7").style.display = "block";
+      document.getElementById("headlist8").style.display = "block";
+      document.getElementById("aksiyaa2").style.display = "none";
+      document.getElementById("aksiyaa3").style.display = "none";
+  
+    });
+  
+    document.getElementById("top").addEventListener("click", () => {
+      document.body.style.paddingLeft = "0";
+      document.body.style.transition = "all 0.3s linear";
+      document.getElementById("aksiyaa").style.display = "none";
+      document.getElementById("headlist1").style.display = "block";
+      document.getElementById("headlist2").style.display = "block";
+      document.getElementById("headlist3").style.display = "block";
+      document.getElementById("headlist4").style.display = "block";
+      document.getElementById("headlist5").style.display = "block";
+      document.getElementById("headlist6").style.display = "block";
+      document.getElementById("headlist7").style.display = "block";
+      document.getElementById("headlist8").style.display = "block";
+      document.getElementById("aksiyaa2").style.display = "none";
+      document.getElementById("aksiyaa3").style.display = "none";
+    });
+    
+  
+    document.getElementById("telefon").addEventListener("click", () => {
+      document.body.style.paddingTop = "0";
+      document.body.style.transition = "all 0.8s linear";
+      document.getElementById("aksiyaa2").style.display = "block";  
+      document.getElementById("aksiyaa3").style.display = "block";
+    });
